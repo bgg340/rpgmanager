f0fcf8a2c5491f57c515c3ece1996e6c96904625
unknown <mikko.nikku@myy-haaga-helia.fi>
2019-06-13 21:23:32 +0300

    starting to look good

--- src/main/java/com/proj/rpgmanager/RpgmanagerApplication.java
+++ src/main/java/com/proj/rpgmanager/RpgmanagerApplication.java
@@ -28,9 +28,11 @@ public class RpgmanagerApplication {
 		
 		Group group1 = new Group("Main party");
 		Group group2 = new Group("Sidequesters");
+		Group group3 = new Group("Unassigned");
 		
 		groupRepository.save(group1);
 		groupRepository.save(group2);
+		groupRepository.save(group3);
 		
 		Character char1 = new Character("Henk", "fighter", "human", "todellinen perusjamppa", 1, 12, 13, 14, 10, 12, 13, 12, 10, "this man has no backstory because he is dull and basic", group1);
 		Character char2 = new Character("Vold", "Monk", "human", "turhake", 1, 12, 13, 14, 10, 12, 13, 12, 10, "This man used to be a baker but for some reason he wanted to become a hero. Now he kicks ass and stuff", group1);
@@ -41,9 +43,9 @@ public class RpgmanagerApplication {
 		characterRepository.save(char3);
 		
 
-		Player p1 = new Player("Harri", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char1);
-		Player p2 = new Player("Joonas", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char2);
-		Player p3 = new Player("Henna", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char3);
+		Player p1 = new Player("harri", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char1);
+		Player p2 = new Player("joonas", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char2);
+		Player p3 = new Player("henna", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char3);
 		Player p4 = new Player("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", char1);
 		
 		playerRepository.save(p1);
