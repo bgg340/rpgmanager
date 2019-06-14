c3b48fd6308d8e4fd17a88638f411203398834a7
unknown <mikko.nikku@myy-haaga-helia.fi>
2019-06-14 19:25:45 +0300

    maybe final fixes

--- src/main/java/com/proj/rpgmanager/RpgmanagerApplication.java
+++ src/main/java/com/proj/rpgmanager/RpgmanagerApplication.java
@@ -46,13 +46,14 @@ public class RpgmanagerApplication {
 		Character char2 = new Character("Vold", "Monk", "human", "turhake", 1, 12, 13, 14, 10, 12, 13, 12, 10, "This man used to be a baker but for some reason he wanted to become a hero. Now he kicks ass and stuff", group1);
 		Character char3 = new Character("Yrgl", "Wizard", "dwarf", "ahistunu jäbä", 1, 12, 13, 14, 10, 12, 13, 12, 10, "lorem ipsum massiivinen backstory, synkkä menneisyys liiba laaba lässyn lää. orpo ja rampa ja kaikki päin persettä", group2);	
 		
+		
 		characterRepository.save(char1);
 		characterRepository.save(char2);
 		characterRepository.save(char3);
 		
 
 		//passwords hashed. users have "user" and admin has "admin".
-		Player p1 = new Player("harri", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char1);
+		Player p1 = new Player("harri", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", null);
 		Player p2 = new Player("joonas", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char2);
 		Player p3 = new Player("henna", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", char3);
 		Player p4 = new Player("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", char1);
--- src/main/java/com/proj/rpgmanager/web/CharacterController.java
+++ src/main/java/com/proj/rpgmanager/web/CharacterController.java
@@ -1,6 +1,5 @@
 package com.proj.rpgmanager.web;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -18,149 +17,142 @@ import com.proj.rpgmanager.domain.GroupRepository;
 import com.proj.rpgmanager.domain.Player;
 import com.proj.rpgmanager.domain.PlayerRepository;
 
-
-
 //the main controller of the program
 @Controller
 public class CharacterController {
-		@Autowired
-		private CharacterRepository characterRepository;
-		
-		@Autowired
-		private GroupRepository groupRepository;
-		
-		@Autowired
-		private PlayerRepository playerRepository;
-		
-		
-	//the character list. gets all required info from said repositories.
-		@RequestMapping("/charlist")
-		public String characterList(Model model) {
-			model.addAttribute("characters", characterRepository.findAll());
-			model.addAttribute("groups", characterRepository.findAll());
-			return "charlist";
-		}
-		
-	//same as above but for a list of players.	
-		@RequestMapping("/playerlist")
-		public String playerrList(Model model) {
-			model.addAttribute("players", playerRepository.findAll());
-			model.addAttribute("characters", characterRepository.findAll());
-			model.addAttribute("groups", characterRepository.findAll());
-			return "playerlist";
-		}
-		
-		
-		//for viewing one character at a time. uses pathVariable to find right one through id
-		@RequestMapping("/charactersheet/{id}")
-		public String characterSheet(@PathVariable("id") long id, Model model) {
-			model.addAttribute("characters", characterRepository.findAll());
-			model.addAttribute("groups", characterRepository.findAll());
-			return "charactersheet";
-		}
-		
-		//for creating new characters
-		@RequestMapping(value = "/add")
-		public String addCharacter(Model model) {
-			model.addAttribute("character", new Character());
-			model.addAttribute("groups", groupRepository.findAll());
-			return "addcharacter";
-		}
-		
-		//same as add, but this was a test for making a separate one for non-admins. this might be useless?
-				@RequestMapping(value = "/useradd")
-				public String addCharacterAsUser(Model model) {
-					model.addAttribute("character", new Character());
-					model.addAttribute("groups", groupRepository.findAll());
-					return "addcharacter";
-				}
-		
-		
-		//edit
-		@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-		public String editCharacter(@PathVariable("id") long id, Model model) {
+	@Autowired
+	private CharacterRepository characterRepository;
 
-			model.addAttribute("character", characterRepository.findById(id));
-			model.addAttribute("groups", groupRepository.findAll());
-			return "editchar";
-		}
-		
-		
-		//for saving new characters or edits to old ones
-		@RequestMapping(value = "/save", method = RequestMethod.POST)
-		public String save(Character character) {
-			characterRepository.save(character);
-			return "redirect:charlist";
-		}
-		
-		//for saving player edits.
-		@RequestMapping(value = "/saveplr", method = RequestMethod.POST)
-		public String saveplayer(Player player) {
-			playerRepository.save(player);
-			
-			return "redirect:playerlist";
-		}
-		
-		//delete a character
-		@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-		public String deleteCharacter(@PathVariable("id") long id, Model model) {
-			characterRepository.deleteById(id);
-			return "redirect:../charlist";
-		}
-		
-		//delete a user/player.
-		@RequestMapping(value = "/deleteplayer/{id}", method = RequestMethod.GET)
-		public String deletePlayer(@PathVariable("id") long id, Model model) {
-			playerRepository.deleteById(id);
-			return "redirect:../playerlist";
-		}
-		
-		
-		//for editing players. only meant for changing their characters, but you could do stupid stuff with this...
-		@RequestMapping(value = "/editplr/{id}", method = RequestMethod.GET)
-		public String editPlayer(@PathVariable("id") long id, Model model) {
-			
-			model.addAttribute("player", playerRepository.findById(id));
-			model.addAttribute("characters", characterRepository.findAll());
-			return "editplayer";
-		}
-		
-		
-		
-		
-        //restful stuff.
-		
-		//you can see all the characters and more here. Should probably be locked somehow...
-		@RequestMapping(value = "/characters", method=RequestMethod.GET)
-		@ResponseBody
-		public List<Character> characterListRest() {
-			return (List<Character>) characterRepository.findAll();
-		}
-		
-		//view one character based on id
-		@RequestMapping(value = "/character/{id}", method=RequestMethod.GET)
-		@ResponseBody
-		public Optional<Character> findCharacterRest(@PathVariable("id") Long id){
-			return characterRepository.findById(id);
-		}
-		
-		//rest delete tool
-		@RequestMapping(value = "/character/{id}", method=RequestMethod.DELETE)
-		@ResponseBody
-		public String deleteRest(@PathVariable("id") Long id){
-			
-			String characterName = characterRepository.findById(id).get().getName();
-			
-			characterRepository.deleteById(id);
-			
-			return characterName + " deleted";
-		}
-		
-		
-		//login request
-		 @RequestMapping(value="/login")
-		  public String login() {
-		    return "login";
-		  }
-		
+	@Autowired
+	private GroupRepository groupRepository;
+
+	@Autowired
+	private PlayerRepository playerRepository;
+
+	// the character list. gets all required info from said repositories.
+	@RequestMapping("/charlist")
+	public String characterList(Model model) {
+		model.addAttribute("characters", characterRepository.findAll());
+		model.addAttribute("groups", characterRepository.findAll());
+		return "charlist";
+	}
+
+	// same as above but for a list of players.
+	@RequestMapping("/playerlist")
+	public String playerrList(Model model) {
+		model.addAttribute("players", playerRepository.findAll());
+		model.addAttribute("characters", characterRepository.findAll());
+		model.addAttribute("groups", characterRepository.findAll());
+		return "playerlist";
+	}
+
+	// for viewing one character at a time. uses pathVariable to find right one
+	// through id
+	@RequestMapping("/charactersheet/{id}")
+	public String characterSheet(@PathVariable("id") long id, Model model) {
+		model.addAttribute("characters", characterRepository.findAll());
+		model.addAttribute("groups", characterRepository.findAll());
+		return "charactersheet";
+	}
+
+	// for creating new characters
+	@RequestMapping(value = "/add")
+	public String addCharacter(Model model) {
+		model.addAttribute("character", new Character());
+		model.addAttribute("groups", groupRepository.findAll());
+		return "addcharacter";
+	}
+
+	// same as add, but this was a test for making a separate one for non-admins.
+	// this might be useless?
+	@RequestMapping(value = "/useradd")
+	public String addCharacterAsUser(Model model) {
+		model.addAttribute("character", new Character());
+		model.addAttribute("groups", groupRepository.findAll());
+		return "addcharacter";
+	}
+
+	// edit
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editCharacter(@PathVariable("id") long id, Model model) {
+
+		model.addAttribute("character", characterRepository.findById(id));
+		model.addAttribute("groups", groupRepository.findAll());
+		return "editchar";
+	}
+
+	// for saving new characters or edits to old ones
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Character character) {
+		characterRepository.save(character);
+		return "redirect:charlist";
+	}
+
+	// for saving player edits.
+	@RequestMapping(value = "/saveplr", method = RequestMethod.POST)
+	public String saveplayer(Player player) {
+		playerRepository.save(player);
+
+		return "redirect:playerlist";
+	}
+
+	// delete a character
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteCharacter(@PathVariable("id") long id, Model model) {
+		characterRepository.deleteById(id);
+		return "redirect:../charlist";
+	}
+
+	// delete a user/player.
+	@RequestMapping(value = "/deleteplayer/{id}", method = RequestMethod.GET)
+	public String deletePlayer(@PathVariable("id") long id, Model model) {
+		playerRepository.deleteById(id);
+		return "redirect:../playerlist";
+	}
+
+	// for editing players. only meant for changing their characters, but you could
+	// do stupid stuff with this...
+	@RequestMapping(value = "/editplr/{id}", method = RequestMethod.GET)
+	public String editPlayer(@PathVariable("id") long id, Model model) {
+
+		model.addAttribute("player", playerRepository.findById(id));
+		model.addAttribute("characters", characterRepository.findAll());
+		return "editplayer";
+	}
+
+	// restful stuff.
+
+	// you can see all the characters and more here. Should probably be locked
+	// somehow...
+	@RequestMapping(value = "/characters", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Character> characterListRest() {
+		return (List<Character>) characterRepository.findAll();
+	}
+
+	// view one character based on id
+	@RequestMapping(value = "/character/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public Optional<Character> findCharacterRest(@PathVariable("id") Long id) {
+		return characterRepository.findById(id);
+	}
+
+	// rest delete tool
+	@RequestMapping(value = "/character/{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public String deleteRest(@PathVariable("id") Long id) {
+
+		String characterName = characterRepository.findById(id).get().getName();
+
+		characterRepository.deleteById(id);
+
+		return characterName + " deleted";
+	}
+
+	// login request
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
 }
